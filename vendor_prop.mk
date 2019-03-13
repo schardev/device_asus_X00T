fdea4b923c266fe9b6ef6bb1a1522f805197ba04
Pawin Vongmasa <pawin@google.com>
2019-03-12 19:56:00 -0700

    X00T: Enable Codec2
    Drop framework_manifest.xml part as the hal is already parsed.
    Don't bring in the c2 xml files as CAF squashed the c2
    support in the already existing media codecs.
    
    Change-Id: Ibe051f4b19c2dbb0a208e751a3b7a332a329b447
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -56,6 +56,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.camera.preview.ubwc=0 \
     vendor.camera.aux.packagelist=org.codeaurora.snapcam
 
+# Codec2 switch
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.media.codec2=2
+
 # Charging maximum voltage
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.chg.max_volt_mv=9000
