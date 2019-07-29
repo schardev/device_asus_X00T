f003ebc4b393646216a93e908ef9a57af2823b03
Sandeep Neerudu <sneerudu@codeaurora.org>
2019-07-29 19:41:48 +0530

    X00T: sensors: remove unpermitted namespace of property
    Change-Id: I967c78b422c774e382bfa9f461e44baa7d47454b
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- rootdir/bin/init.qcom.sensors.sh
+++ rootdir/bin/init.qcom.sensors.sh
@@ -31,13 +31,12 @@
 #
 start_sensors()
 {
-    sscrpcd_status=`getprop init.svc.vendor.sensors`
-    chmod -h 664 /persist/sensors/sensors_settings
-    chown -h -R system.system /persist/sensors
+    chmod -h 664 /mnt/vendor/persist/sensors/sensors_settings
+    chown -h -R system.system /mnt/vendor/persist/sensors
     start vendor.sensors.qti
 
     # Only for SLPI
-    if [ -c /dev/msm_dsps -o -c /dev/sensors ] && [ -z "$sscrpcd_status" ]; then
+    if [ -c /dev/msm_dsps -o -c /dev/sensors ]; then
         start vendor.sensors
     fi
 }
