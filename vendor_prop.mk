5f92186bac8ecd9982cbfcb19b108ba55d2ed102
Adhitya Mohan <me@adhityamohan.in>
2019-09-30 14:32:04 +0530

    X00T: Ensure ro.surface_flinger.protected_contents=true is copied to build.prop
    -Having it as DEFAULT_OVERRIDE did not copy the prop, causing chrome v77+ to crash
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -210,7 +210,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
-    ro.surface_flinger.protected_contents=true \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
 
@@ -219,6 +218,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.early_app_phase_offset_ns=1500000 \
     debug.sf.early_gl_phase_offset_ns=3000000 \
     debug.sf.early_gl_app_phase_offset_ns=15000000
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.protected_contents=true
 
 # System prop for UBWC
 PRODUCT_PROPERTY_OVERRIDES += \
