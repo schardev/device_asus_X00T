46907c0f49f1d22c6c2f8d5a3283d6d42324df6e
Saurabh Charde <saurabhchardereal@gmail.com>
2019-12-16 17:07:16 +0530

    X00T: Build vendor variant of libqdMetaData
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

--- device.mk
+++ device.mk
@@ -151,12 +151,15 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
+
+PRODUCT_PACKAGES += \
     vendor.display.config@1.3 \
     copybit.sdm660 \
     gralloc.sdm660 \
     hwcomposer.sdm660 \
     libdisplayconfig \
     liboverlay \
+    libqdMetaData \
     libqdMetaData.system \
     libvulkan \
     libtinyxml \
