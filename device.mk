3b554ee6aa17559fabd226ff21ec49efc6a27856
Stylogey <stylogey@gmail.com>
2020-02-07 20:06:49 +0100

    sdm660-common: Build configstore@1.1
     In manifest it is already set to 1.1

--- device.mk
+++ device.mk
@@ -148,7 +148,7 @@ PRODUCT_PACKAGES += \
 
 # Configstore
 PRODUCT_PACKAGES += \
-    android.hardware.configstore@1.0-service
+    android.hardware.configstore@1.1-service
 
 # Codec2 modules
 PRODUCT_PACKAGES += \
