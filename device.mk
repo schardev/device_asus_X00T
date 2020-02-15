2569a9a43f513cb2d2743bce38568416d79071e0
PIPIPIG233666 <pig.priv@gmail.com>
2020-02-14 22:39:33 -0500

    X00T: Build custom audio amplifier
    Change-Id: I369555a9fcb06065177636b436d2e006c7a0b07d
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

--- device.mk
+++ device.mk
@@ -98,6 +98,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
+    audio_amplifier.sdm660 \
     audio.primary.sdm660 \
     audio.r_submix.default \
     audio.usb.default \
