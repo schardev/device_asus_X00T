a9e1bdd5cb72cf8d9b21318e81de6e522beb3962
Rahul Krishna <Rahulkrishna585@gmail.com>
2020-04-17 22:55:57 +0000

    X00T: Prevent bluetooth logspam when bt headset is connected
    04-18 04:22:25.299 I/vendor.qti.bluetooth@1.0-ibs_handler(3040): ProcessIbsCmd: Writing IBS_WAKE_ACK
    04-18 04:22:25.579 I/vendor.qti.bluetooth@1.0-ibs_handler(3040): ProcessIbsCmd: Received IBS_SLEEP_IND: 0xFE
    04-18 04:22:25.581 I/vendor.qti.bluetooth@1.0-ibs_handler(3040): ProcessIbsCmd: Received IBS_WAKE_IND: 0xFD
    04-18 04:22:25.599 I/vendor.qti.bluetooth@1.0-ibs_handler(3040): ProcessIbsCmd: Writing IBS_WAKE_ACK
    
    Change-Id: Ib74fcc094b824fd66d8c8bfc4ca4ae2ea7bc0ae4

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -90,7 +90,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.bluetooth.soc=cherokee \
     vendor.qcom.bluetooth.soc=cherokee \
     persist.vendor.bt.aac_frm_ctl.enabled=true \
-    persist.vendor.btstack.a2dp_offload_cap=sbc-aptx-aptxhd-aac
+    persist.vendor.btstack.a2dp_offload_cap=sbc-aptx-aptxhd-aac \
+    persist.vendor.service.bdroid.sibs=false
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
