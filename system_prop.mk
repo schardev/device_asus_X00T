0cde4456adbcbc966edb264ebb4300c58d0b56cd
Pranav Kapoor <pranavkapoor001@gmail.com>
2020-05-10 21:30:06 +0530

    X00T: Load media profile using media.settings prop
    * Fails otherwise
    
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -3,3 +3,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase.ms=android-asus-tpin \
     ro.com.google.rlzbrandcode=ASUP \
     ro.com.google.rlz_ap_whitelist=y0,y5,y6,y7,y9
+
+# Media
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    media.settings.xml=/vendor/etc/media_profiles_vendor.xml
