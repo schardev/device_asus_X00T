5b338beb1c2116b8977e36e891a3ad7ed638eb59
Saurabh Charde <saurabhchardereal@gmail.com>
2020-06-02 15:10:45 +0530

    X00T: Drop nvram_btwifi
    * Asus (AMT) has dropped it in Stock Android 10
    * Also, ro.btmac prop is setting up properly without it now
    
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -4,14 +4,6 @@ include $(CLEAR_VARS)
 # bin
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.btmac.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := bin/init.btmac.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.post_boot.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
