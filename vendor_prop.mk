613f3d1601ae45764dd73d2b7e694e8af2e7f625
Saurabh Charde <saurabhchardereal@gmail.com>
2020-08-09 12:02:54 +0530

    X00T: prop: Do not force enable VoLTE
    * since most carrier restrictions have been addressed already
      via CarrierConfig
    
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -290,7 +290,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # IMS
 PRODUCT_PROPERTY_OVERRIDES += \
-   persist.dbg.ims_volte_enable=1 \
    persist.dbg.volte_avail_ovr=1 \
    persist.dbg.vt_avail_ovr=1 \
    persist.dbg.wfc_avail_ovr=1 \
