056106815c2c8d2e48b1b18dbc999144267dc9ff
Himanshu Agrawal <hagraw@codeaurora.org>
2020-08-17 13:07:28 +0530

    X00T: prop: Disable ahardware buffer
    - Disable ahardware buffer usage in gralloc
    
    Change-Id: I8020828b12a6c78407e35486a32b37aaedfdbf7e
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -143,6 +143,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.hw=1 \
     debug.sf.latch_unsignaled=1 \
     vendor.gralloc.enable_fb_ubwc=1 \
+    vendor.gralloc.disable_ahardware_buffer=1 \
     debug.hwui.use_buffer_age=false \
     dev.pm.dyn_samplingrate=1 \
     persist.demo.hdmirotationlock=false \
