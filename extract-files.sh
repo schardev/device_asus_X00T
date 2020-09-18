883b1b0f47b8acd30353735229e8a6564fa4aab7
Saurabh Charde <saurabhchardereal@gmail.com>
2020-09-18 11:10:34 +0530

    X00T: Patch few blobs to load v29 libprotobuf-cpp-{full,lite}.so
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -59,6 +59,28 @@ if [ -z "${SRC}" ]; then
     SRC=adb
 fi
 
+function blob_fixup() {
+    case "${1}" in
+
+    vendor/lib64/libril-qc-hal-qmi.so)
+        patchelf --replace-needed "libprotobuf-cpp-full.so" "libprotobuf-cpp-full-v29.so" "${2}"
+        ;;
+
+    vendor/lib64/libsettings.so)
+        patchelf --replace-needed "libprotobuf-cpp-full.so" "libprotobuf-cpp-full-v29.so" "${2}"
+        ;;
+
+    vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so)
+        patchelf --replace-needed "libprotobuf-cpp-lite.so" "libprotobuf-cpp-lite-v29.so" "${2}"
+        ;;
+
+    vendor/lib64/libwvhidl.so)
+        patchelf --replace-needed "libprotobuf-cpp-lite.so" "libprotobuf-cpp-lite-v29.so" "${2}"
+        ;;
+
+    esac
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false "${CLEAN_VENDOR}"
 
