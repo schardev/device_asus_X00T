3686a5a4829a7c7cb7c77affde499c77d60eb33e
PIPIPIG233666 <pig.priv@gmail.com>
2020-11-01 17:51:18 -0500

    X00T: Split component-overrides to support pure aosp/qti bt stack
    Change-Id: Ia40a996ba5164ba8a6fc5dde0dfb3ef859af1477
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

--- device.mk
+++ device.mk
@@ -150,7 +150,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
+    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
+    $(LOCAL_PATH)/configs/component-overrides_qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/component-overrides.xml
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2160
