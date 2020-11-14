d61e8038003a7005b0227baa92a2879ee4431f09
Saurabh Charde <saurabhchardereal@gmail.com>
2020-11-14 19:47:56 +0530

    X00T: Fix qcom sepolicy inclusion
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -192,7 +192,7 @@ BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 VENDOR_SECURITY_PATCH := 2020-04-05
 
 # SELinux
-include device/qcom/sepolicy-legacy-um/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Treble
