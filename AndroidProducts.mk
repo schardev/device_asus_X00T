c2af2b9fc9c68b8eb9e15f961c05fc75b1687dea
Saurabh Charde <saurabhchardereal@gmail.com>
2021-04-21 16:09:21 +0530

    X00T: Havoc Bringup
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_X00T.mk
+    $(LOCAL_DIR)/havoc_X00T.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_X00T-user \
-    arrow_X00T-userdebug \
-    arrow_X00T-eng
+    havoc_X00T-user \
+    havoc_X00T-userdebug \
+    havoc_X00T-eng
--- device.mk
+++ device.mk
@@ -224,7 +224,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-arrow
+    $(LOCAL_PATH)/overlay-havoc
 
 # Overlays - Enforce RRO
 PRODUCT_ENFORCE_RRO_TARGETS := *
